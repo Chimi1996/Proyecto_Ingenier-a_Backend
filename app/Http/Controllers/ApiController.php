a7086979024d070f37eb214a783e00e6252c9d0a
Iver Gabriel Picado Rojas <IVER.PICADO@ucr.ac.cr>
2024-06-17 21:46:53 -0600

    Creacion de ApiController y rutas con primer metodo

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Driver;
+use App\Models\Passenger;
+use App\Models\Trip;
+use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\VehicleDetail;
+use Illuminate\Support\Facades\Hash;
+
+
+class ApiController extends Controller
+{
+
+    public function ObtenerViajes()
+    {
+        $Trips = Trip::all();
+
+        return response()->json($Trips);
+    }
+
+    
+
+
+
+
+
+    
+}
